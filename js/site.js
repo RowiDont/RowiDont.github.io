177e836957e5dcde3d497472c764b47ea8e392d1
Rafael P <rafiepatel@gmail.com>
2016-04-12 12:15:01 -0400

    Add bio with hide/unhide functionality.

--- js/site.js
+++ js/site.js
@@ -1,20 +1,39 @@
-function changeProject(e) {
-  var data = e.currentTarget.dataset.key;
+var changeProject = function(e) {
+  var data = e.currentTarget.dataset.key
 
-  var selectedList = $(e.currentTarget);
-  var selectedItem;
+  var selectedList = $(e.currentTarget)
+  var selectedItem
   items.each(function (idx, el) {
     if (el.dataset.key === data) { selectedItem = $(el); }
-  });
+  })
 
-  list.removeClass('list-selected');
-  items.addClass('hidden');
+  list.removeClass('list-selected')
+  items.addClass('hidden')
 
-  selectedList.addClass('list-selected');
-  selectedItem.removeClass('hidden');
+  selectedList.addClass('list-selected')
+  selectedItem.removeClass('hidden')
 }
 
-var list = $('.list li');
-var items = $('.project-content > li');
+var showHideBio = function(e) {
+  var state = e.currentTarget.dataset.key
+  var el = $('#bio')
+  var link = $(e.currentTarget)
 
-list.on('click', changeProject);
+  // Get data key.
+  // If key === "expand", remove hidden class from h3, otherwise add it.
+
+  if (state === "expand") {
+    el.removeClass('hidden')
+    link.attr('data-key', 'hide')
+  } else {
+    el.addClass('hidden')
+    link.attr('data-key', 'expand')
+  }
+}
+
+var list = $('.list li')
+var items = $('.project-content > li')
+var expander = $('#expand')
+
+list.on('click', changeProject)
+expander.on('click', showHideBio)
